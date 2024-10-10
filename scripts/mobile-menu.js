dd6844192aee265d83f51455c42074fb95a30bbe
Clemence <clem.tafforeau@gmail.com>
2024-10-10 14:19:58 +0200

    V1 (static version) and minor fixes

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,4 +1,5 @@
-const main = document.querySelector('main')
+const main = document.querySelector('main');
+const header = document.querySelector('header');
 const menu = document.querySelector('.menu-btn');
 const nav = document.querySelector('.navigation');
 
@@ -8,4 +9,12 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('opened');
     menu.setAttribute('aria-expanded', menu.classList.contains('opened'));
     nav.setAttribute('aria-expanded', menu.classList.contains('opened'));
+})
+
+window.addEventListener('scroll', () => {
+    if(scrollY > 0) {
+        header.classList.add('blurred-backdrop');
+    } else {
+        header.classList.remove('blurred-backdrop');
+    }
 })
